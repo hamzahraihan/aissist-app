b12b378b192659512d7b55fb3651e2454fbef6b1
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-22 14:56:35 +0700

    add HomeLayout component with stack navigation and custom header styles

---
+++ app/(tabs)/(home)/_layout.tsx
@@ -0,0 +1,20 @@
+import { Stack } from 'expo-router';
+
+export default function HomeLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#ad4545',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+    >
+      <Stack.Screen name="index" />
+      <Stack.Screen name="details" />
+    </Stack>
+  );
+}
