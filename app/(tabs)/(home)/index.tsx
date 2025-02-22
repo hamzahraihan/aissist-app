59a5f241b5e2e25e55dcf8a972b22a01d987f08d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-22 14:56:44 +0700

    add HomeScreen component with basic layout and navigation link to Details

---
+++ app/(tabs)/(home)/index.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Edited.</Text>
+      <Link href="/details">Details</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
