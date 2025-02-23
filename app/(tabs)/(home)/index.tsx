cbbd002d0e07177a37ede1cc8760be23d699ddb6
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-23 10:06:18 +0700

    fix: update HomeScreen links to include dynamic ID parameters and improve styling

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -5,7 +5,13 @@ export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text>Edited.</Text>
-      <Link href="/details">Details</Link>
+
+      <Link style={styles.linkButton} href="/details/1">
+        Details
+      </Link>
+      <Link style={styles.linkButton} href="/details/2">
+        Details
+      </Link>
     </View>
   );
 }
@@ -15,5 +21,14 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    gap: 2,
+  },
+  linkButton: {
+    color: 'white',
+    backgroundColor: '#352da3',
+    fontSize: 20,
+    borderRadius: 8,
+    cursor: 'pointer',
+    padding: 12,
   },
 });
