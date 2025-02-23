173f0b75338ebdd37b1c68aa080fa881c01e5c0d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-23 10:06:10 +0700

    fix: update details screen route to include dynamic ID parameter

--- app/(tabs)/(home)/_layout.tsx
+++ app/(tabs)/(home)/_layout.tsx
@@ -14,7 +14,7 @@ export default function HomeLayout() {
       }}
     >
       <Stack.Screen name="index" />
-      <Stack.Screen name="details" />
+      <Stack.Screen name="details/[id]" />
     </Stack>
   );
 }
