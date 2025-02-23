a3098dd844a8b7a85e2436f90413ea26a735e44a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-23 10:06:06 +0700

    fix: remove unnecessary screenOptions from TabLayout component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,7 @@ import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs>
       <Tabs.Screen name="(home)" />
       <Tabs.Screen name="settings" />
     </Tabs>
