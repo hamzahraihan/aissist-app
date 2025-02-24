94536689ff9091d31327ae9c00c7d6baebe4f858
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-24 10:46:16 +0700

    add: implement custom font loading and splash screen handling in RootLayout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,27 @@
-import { Stack } from 'expo-router';
+import { fonts } from '@/constants/Colors';
+import { useFonts } from '@expo-google-fonts/inter';
+import { SplashScreen, Stack } from 'expo-router';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    fonts,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync;
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)" />
     </Stack>
   );
