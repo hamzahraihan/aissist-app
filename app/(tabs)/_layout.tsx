83e53f9e566fdc9490020b408b0147f879680b39
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-24 10:46:20 +0700

    add: enhance TabLayout with custom tab bar options and icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,25 @@
+import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
+import { Pressable } from 'react-native';
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="(home)" />
-      <Tabs.Screen name="settings" />
+    <Tabs
+      screenOptions={{
+        tabBarButton: (props) => <Pressable {...props} android_ripple={{ color: 'transparent' }} />,
+        tabBarActiveTintColor: 'black',
+        tabBarStyle: { height: 70 },
+        tabBarIconStyle: { height: 40 },
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen name="index" options={{ title: 'Home', tabBarIcon: ({ color }) => <Ionicons size={32} name="home" color={color} /> }} />
+
+      <Tabs.Screen name="chat" options={{ title: 'Chat', tabBarIcon: ({ color }) => <Ionicons size={32} name="chatbubble" color={color} /> }} />
+
+      <Tabs.Screen name="images" options={{ title: 'Images', tabBarIcon: ({ color }) => <Ionicons size={32} name="image" color={color} /> }} />
+
+      <Tabs.Screen name="settings" options={{ title: 'Settings', tabBarIcon: ({ color }) => <Ionicons size={32} name="settings" color={color} /> }} />
     </Tabs>
   );
 }
