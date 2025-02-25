e5e01efa6f93062556e21a2a2c23a27149072a5f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-25 12:30:27 +0700

    feat: implement useTheme hook for dynamic theme-based color retrieval

---
+++ context/useTheme.tsx
@@ -0,0 +1,13 @@
+import { darkTheme, lightTheme } from '@/constants/theme';
+import { useColorScheme } from 'react-native';
+
+export function useTheme(props: { light?: string; dark?: string }, colorName: keyof typeof lightTheme & keyof typeof darkTheme): any {
+  const theme = useColorScheme() ?? 'light';
+  const colorFromProps = props[theme];
+
+  if (colorFromProps) {
+    return colorFromProps;
+  } else {
+    return theme == 'light' ? lightTheme[colorName] : darkTheme[colorName];
+  }
+}
