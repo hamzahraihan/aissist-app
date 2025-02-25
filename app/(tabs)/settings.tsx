df30ac7f726acf680b58a1f3e4e36437d51ea522
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-25 12:30:02 +0700

    feat: implement dark mode toggle in SettingsScreen with themed components

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,17 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { CustomSwitch } from '@/components/Switch';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { Ionicons } from '@expo/vector-icons';
+import { useState } from 'react';
+import { Appearance, StyleSheet, useColorScheme, View } from 'react-native';
 
 export default function SettingsScreen() {
+  const [isEnabled, setIsEnabled] = useState<boolean>();
+  const colorScheme = useColorScheme();
+
+  const toggleSwitch = () => {
+    if (isEnabled) {
+      Appearance.setColorScheme('light');
+    } else {
+      Appearance.setColorScheme('dark');
+    }
+    setIsEnabled((previousState) => !previousState);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Settings</Text>
-    </View>
+    <ThemedView style={styles.container}>
+      <View style={[styles.flexRow, { margin: 10 }]}>
+        <View style={[styles.flexRow, { gap: 10 }]}>
+          <Ionicons name="brush" color={colorScheme == 'light' ? 'black' : 'white'} size={22} />
+          <ThemedText>Dark mode</ThemedText>
+        </View>
+        <CustomSwitch onValueChange={toggleSwitch} value={colorScheme == 'light' ? false : true} isEnabled />
+      </View>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+  },
+  flexRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
 });
