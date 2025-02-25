f5b89fccdaa238fb9750b60a46e0ae6b365fb7c0
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-25 12:29:58 +0700

    feat: update HomeScreen layout with themed components and horizontal scrollable cards

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Card } from '@/components/Card';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { StyleSheet } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default function HomeScreen() {
   return (
-    <>
-      <View style={styles.container}>
-        <Text>Home</Text>
-      </View>
-    </>
+    <ThemedView style={styles.container}>
+      <ThemedText style={{ margin: 10 }} type="title">
+        Home
+      </ThemedText>
+      <ScrollView horizontal contentContainerStyle={{ gap: 20, paddingHorizontal: 20 }} showsHorizontalScrollIndicator={false}>
+        <Card style={styles.card}>
+          <ThemedText type="default">Hello</ThemedText>
+        </Card>
+        <Card style={styles.card}>
+          <ThemedText>Hello</ThemedText>
+        </Card>
+        <Card style={styles.card}>
+          <ThemedText>Hello</ThemedText>
+        </Card>
+      </ScrollView>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  card: {
+    display: 'flex',
+    gap: 10,
+    height: 163,
+    width: 129,
     justifyContent: 'center',
     alignItems: 'center',
   },
