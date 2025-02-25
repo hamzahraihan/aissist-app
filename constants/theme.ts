fd008f7b77054c0db81277cb1c1721d1f1f0d15a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-25 12:30:24 +0700

    feat: add theme constants for light and dark themes with color and font definitions

---
+++ constants/theme.ts
@@ -0,0 +1,54 @@
+const colors = {
+  white: '#fff',
+  black: '#000',
+  gray: 'rgba(0, 0, 0, .5)',
+  lightWhite: 'rgba(255, 255, 255, .5)',
+  blueTintColor: '#0281ff',
+  lightPink: '#F7B5CD',
+  whitesmoke: '#F7F7F8',
+  lightBlack: '#232627',
+};
+
+const fonts = {
+  thinFont: 'Poppins_100Thin',
+  extraLightFont: 'Poppins_200ExtraLight',
+  lightFont: 'Poppins_300Light',
+  regularFont: 'Poppins_400Regular',
+  mediumFont: 'Poppins_500Medium',
+  semiBoldFont: 'Poppins_600SemiBold',
+  boldFont: 'Poppins_700Bold',
+  extraBold: 'Poppins_800ExtraBold',
+  blackFont: 'Poppins_900Black',
+};
+
+const lightTheme = {
+  textColor: colors.black,
+  secondaryTextColor: colors.white,
+  mutedForegroundColor: colors.gray,
+  backgroundColor: colors.white,
+  placeholderTextColor: colors.gray,
+  secondaryBackgroundColor: colors.black,
+  borderColor: 'rgba(0, 0, 0, .15)',
+  tintColor: '#0281ff',
+  tintTextColor: colors.white,
+  tabBarActiveTintColor: colors.black,
+  tabBarInactiveTintColor: colors.gray,
+  cardColor: colors.whitesmoke,
+};
+
+const darkTheme = {
+  textColor: colors.white,
+  secondaryTextColor: colors.black,
+  mutedForegroundColor: colors.lightWhite,
+  backgroundColor: colors.black,
+  placeholderTextColor: colors.lightWhite,
+  secondaryBackgroundColor: colors.white,
+  borderColor: 'rgba(255, 255, 255, .2)',
+  tintColor: '#0281ff',
+  tintTextColor: colors.white,
+  tabBarActiveTintColor: colors.white,
+  tabBarInactiveTintColor: colors.lightWhite,
+  cardColor: colors.lightBlack,
+};
+
+export { colors, fonts, darkTheme, lightTheme };
