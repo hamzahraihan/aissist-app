e63a3947915506a3e1947eeb52eaddc1127ab27d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-26 09:09:27 +0700

    feat: implement chat screen with OpenAI integration and user input handling

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,12 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemedView } from '@/components/ThemedView';
+import { openAiService } from '@/services/openai';
+import { IOpenAIMessage } from '@/types/chat';
+import { useState } from 'react';
+import { Button, StyleSheet, TextInput } from 'react-native';
 
 export default function ChatScreen() {
+  const [input, setInput] = useState<string>('');
+
+  // set messages for ai
+  const [openAiMessages, setOpenAiMessages] = useState<IOpenAIMessage[]>([]);
+
+  const generateTextFromOpenAI = async () => {
+    if (input) {
+      setOpenAiMessages([{ role: 'user', content: input }]);
+    }
+    const data = await openAiService(openAiMessages);
+    console.log(data);
+  };
+
   return (
-    <>
-      <View style={styles.container}>
-        <Text>Chat</Text>
-      </View>
-    </>
+    <ThemedView style={styles.container}>
+      <TextInput
+        style={{ borderWidth: 1, borderColor: 'white', height: 40, padding: 12, color: 'white' }}
+        onChangeText={setInput}
+        value={input}
+        placeholder="Input your prompt"
+        placeholderTextColor="white"
+        selectionColor="white"
+        cursorColor="white"
+      />
+      <Button title="Press me" color="#f194ff" onPress={() => generateTextFromOpenAI()} />
+    </ThemedView>
   );
 }
 
