4f2a0ee7aeb8408c2342d0e09285b51675832114
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-26 14:09:26 +0700

    feat: refactor openAiService to use OpenAI SDK and update message handling

--- services/openai.ts
+++ services/openai.ts
@@ -1,27 +1,43 @@
-import { OPENAI_API_KEY } from '@env';
+import { OPENAI_API_KEY } from 'react-native-dotenv';
+import OpenAI from 'openai';
+import { ChatCompletionMessageParam } from 'openai/resources';
 
-import { IOpenAIMessage } from '@/types/chat';
-export async function openAiService(messages: IOpenAIMessage[]) {
+const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+
+export async function openAiService(messages: ChatCompletionMessageParam[]): Promise<string | undefined> {
+  // try {
+  //   if (messages.some((message) => message.role == '' && message.content == '')) {
+  //     throw new Error('User not input anything');
+  //   }
+
+  //   const response = await fetch('https://api.openai.com/v1/chat/completions', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       Authorization: `Bearer ${OPENAI_API_KEY}`,
+  //     },
+  //     body: JSON.stringify({
+  //       model: 'gpt-4o-mini',
+  //       messages,
+  //       store: true,
+  //     }),
+  //   });
+  //   const data = await response.json();
+  //   return data;
+  // } catch (error: any) {
+  //   throw new Error(error);
+  // }
   try {
-    if (messages.some((message) => message.role == '' && message.content == '')) {
-      throw new Error('User not input anything');
-    }
-
-    const response = await fetch('https://api.openai.com/v1/chat/completions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${OPENAI_API_KEY}`,
-      },
-      body: JSON.stringify({
-        model: 'gpt-4o-mini',
-        messages,
-        stream: true,
-      }),
+    console.log('Request sent to OpenAI...');
+    const completion = await openai.chat.completions.create({
+      model: 'gpt-4o-mini',
+      messages: [{ role: 'developer', content: 'You are a helpful assistant' }, ...messages],
+      stream: false,
+      store: true,
     });
-    const data = await response.json();
-    return data;
-  } catch (error: any) {
-    throw new Error(error);
+
+    return completion.choices[0]?.message?.content || '';
+  } catch (error) {
+    console.error('OpenAI API Error:', error);
   }
 }
