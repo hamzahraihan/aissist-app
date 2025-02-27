d8ae58f03baf251a5aa13c34fe90b9ce65cb396b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-27 11:25:49 +0700

    feat: add CustomButton component with theme support for light and dark modes

---
+++ components/Button.tsx
@@ -0,0 +1,12 @@
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { useTheme } from '@/context/useTheme';
+
+export type CustomButtonProps = TouchableOpacityProps & {
+  lightColor?: string;
+  darkColor?: string;
+};
+
+export function CustomButton({ style, lightColor, darkColor, ...props }: CustomButtonProps) {
+  const backgroundColor = useTheme({ light: lightColor, dark: darkColor }, 'cardColor');
+  return <TouchableOpacity style={[{ backgroundColor }, style]} {...props} />;
+}
