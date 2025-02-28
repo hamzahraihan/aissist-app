bf9671ba982bfdf6e2c7fa5296845b7c1b43cef8
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-28 15:29:15 +0700

    adjust ThemedView with AIResponse component so the color doesnt disturb each other

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -1,13 +1,25 @@
 import { useTheme } from '@/hooks/useTheme';
-import { View, type ViewProps } from 'react-native';
+import { StyleSheet, View, type ViewProps } from 'react-native';
 
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
   darkColor?: string;
+  type?: 'developer' | 'assistant' | 'user';
 };
 
-export function ThemedView({ style, lightColor, darkColor, ...props }: ThemedViewProps) {
+export function ThemedView({ style, lightColor, darkColor, type = 'user', ...props }: ThemedViewProps) {
   const backgroundColor = useTheme({ light: lightColor, dark: darkColor }, 'backgroundColor');
+  const assistantBackground = useTheme({ light: lightColor, dark: darkColor }, 'cardColor');
+  const userBackground = useTheme({ light: lightColor, dark: darkColor }, 'backgroundColor');
 
-  return <View style={[{ backgroundColor }, style]} {...props} />;
+  const styles = StyleSheet.create({
+    assistant: {
+      backgroundColor: assistantBackground,
+    },
+    user: {
+      backgroundColor: userBackground,
+    },
+  });
+
+  return <View style={[{ backgroundColor }, type === 'assistant' ? styles.assistant : styles.user, style]} {...props} />;
 }
