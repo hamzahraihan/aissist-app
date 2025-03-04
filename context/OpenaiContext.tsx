bfaa44cc9720ec8d16891ab82017575ff5b771b3
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-03-04 14:44:26 +0700

    feat: enhance OpenaiContext to manage chat history and UUID for messages

--- context/OpenaiContext.tsx
+++ context/OpenaiContext.tsx
@@ -1,27 +1,58 @@
 import { openAiService } from '@/services/openai';
 import { ChatCompletionMessageParam } from 'openai/resources';
 import { createContext, type Dispatch, ReactNode, useState } from 'react';
+import uuid from 'react-native-uuid';
+
+export type ChatMessageProps = {
+  uuid: any;
+  message: ChatCompletionMessageParam[];
+  createdAt: string;
+};
 
 export const OpenaiContext = createContext<{
   generateTextFromOpenAI: () => Promise<void>;
   loading: boolean;
   setInput: Dispatch<React.SetStateAction<string>>;
   input: string;
-  openAiMessages: ChatCompletionMessageParam[];
+  openAiMessages: ChatMessageProps;
+  chatHistory: ChatMessageProps[];
+  setChatHistory: Dispatch<React.SetStateAction<ChatMessageProps[]>>;
+  saveChatHistory: () => void;
 }>({
   generateTextFromOpenAI: async () => {},
   loading: false,
   input: '',
   setInput: () => {},
-  openAiMessages: [{ role: 'developer', content: 'You are a helpful assistant!' }],
+  openAiMessages: { uuid: '', message: [], createdAt: new Date().toLocaleString() },
+  chatHistory: [],
+  setChatHistory: () => {},
+  saveChatHistory: () => {},
 });
 
 export const OpenaiProvider = ({ children }: { children: ReactNode }) => {
   const [input, setInput] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
-  console.log('Loading state from openai context', loading);
+
+  const date = Date.now();
+
+  const formatDate = (date: number): string => {
+    const options: any = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    };
+    return new Date(date).toLocaleDateString(undefined, options);
+  };
+
   // set messages for ai
-  const [openAiMessages, setOpenAiMessages] = useState<ChatCompletionMessageParam[]>([]);
+  const [openAiMessages, setOpenAiMessages] = useState<ChatMessageProps>({
+    uuid: '',
+    createdAt: formatDate(date),
+    message: [],
+  });
+  const [chatHistory, setChatHistory] = useState<ChatMessageProps[]>([{ uuid: '', message: [], createdAt: formatDate(date) }]);
+  console.log(chatHistory);
   console.log('🚀 ~ OpenaiProvider ~ openAiMessages:', openAiMessages);
 
   const generateTextFromOpenAI = async (): Promise<void> => {
@@ -29,19 +60,19 @@ export const OpenaiProvider = ({ children }: { children: ReactNode }) => {
     try {
       if (!input.trim()) {
         console.log('Input is empty, resetting messages...');
-        setOpenAiMessages([]);
+        setOpenAiMessages({ message: [], uuid: '', createdAt: formatDate(date) });
         return;
       }
 
       console.log('User input:', input);
 
       // Append the new user message while keeping previous messages
-      setOpenAiMessages((prevMessages) => [...prevMessages, { role: 'user', content: input.trim() }]);
+      setOpenAiMessages((prevMessages) => ({ uuid: uuid.v4(), createdAt: formatDate(date), message: [...prevMessages.message, { role: 'user', content: input.trim() }] }));
 
       setInput(''); // Clear input field
 
       // Get response from OpenAI
-      const responseText = await openAiService([...openAiMessages, { role: 'user', content: input.trim() }]);
+      const responseText = await openAiService([...openAiMessages.message, { role: 'user', content: input.trim() }]);
 
       if (!responseText) {
         console.warn('No response received from OpenAI.');
@@ -49,7 +80,8 @@ export const OpenaiProvider = ({ children }: { children: ReactNode }) => {
       }
 
       // Append OpenAI's response to the chat history
-      setOpenAiMessages((prevMessages) => [...prevMessages, { role: 'assistant', content: responseText }]);
+      setOpenAiMessages((prevMessages) => ({ uuid: prevMessages.uuid, createdAt: formatDate(date), message: [...prevMessages.message, { role: 'assistant', content: responseText }] }));
+
       console.log('OpenAI Response:', responseText);
       setLoading(false);
     } catch (error) {
@@ -59,5 +91,11 @@ export const OpenaiProvider = ({ children }: { children: ReactNode }) => {
     }
   };
 
-  return <OpenaiContext.Provider value={{ generateTextFromOpenAI, loading, setInput, input, openAiMessages }}>{children}</OpenaiContext.Provider>;
+  const saveChatHistory = () => {
+    setChatHistory((prev) => [...prev, { uuid: openAiMessages.uuid, createdAt: formatDate(date), message: openAiMessages.message }]);
+
+    setOpenAiMessages({ uuid: '', createdAt: formatDate(date), message: [] });
+  };
+
+  return <OpenaiContext.Provider value={{ generateTextFromOpenAI, loading, setInput, input, openAiMessages, saveChatHistory, chatHistory, setChatHistory }}>{children}</OpenaiContext.Provider>;
 };
