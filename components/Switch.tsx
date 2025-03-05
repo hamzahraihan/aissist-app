8cbd1c419643472e007e75bd259eca625ce2151d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-03-05 16:04:16 +0700

    fix: correct comparison operator in CustomSwitch component for color scheme

--- components/Switch.tsx
+++ components/Switch.tsx
@@ -9,5 +9,5 @@ export type CustomSwitchProps = SwitchProps & {
 export function CustomSwitch({ style, lightColor, darkColor, ...props }: CustomSwitchProps) {
   const colorScheme = useColorScheme();
 
-  return <Switch trackColor={{ true: '#767577', false: '#81b0ff' }} thumbColor={colorScheme == 'light' ? '#f4f3f4' : '#f5dd4b'} ios_backgroundColor="#3e3e3e" {...props} />;
+  return <Switch trackColor={{ true: '#767577', false: '#81b0ff' }} thumbColor={colorScheme === 'light' ? '#f4f3f4' : '#f5dd4b'} ios_backgroundColor="#3e3e3e" {...props} />;
 }
