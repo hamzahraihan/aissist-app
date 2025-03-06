7ffafbf5fe8a7efeba0906996215785cd36692ea
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-03-06 13:29:58 +0700

    feat: update SettingsScreen to manage theme state and toggle color scheme

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,24 +1,32 @@
+/* eslint-disable no-unused-expressions */
 import { CustomSwitch } from '@/components/Switch';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { Ionicons } from '@expo/vector-icons';
 import { useEffect, useState } from 'react';
-import { Appearance, StyleSheet, useColorScheme, View } from 'react-native';
+import { Appearance, StyleSheet, View } from 'react-native';
 
 export default function SettingsScreen() {
-  const [isEnabled, setIsEnabled] = useState<boolean>(false);
-  const colorScheme = useColorScheme();
+  const [theme, setTheme] = useState({ mode: Appearance.getColorScheme() });
+  const colorScheme = Appearance.getColorScheme();
+  console.log(colorScheme);
 
   useEffect(() => {
-    if (isEnabled) {
-      Appearance.setColorScheme('light');
-    } else {
-      Appearance.setColorScheme('dark');
-    }
-  }, [isEnabled]);
+    Appearance.addChangeListener(({ colorScheme }) => {
+      setTheme({ mode: colorScheme });
+    });
+  }, []);
 
-  const toggleSwitch = () => {
-    setIsEnabled((previousState) => !previousState);
+  const toggleSwitch = async () => {
+    try {
+      if (theme.mode === 'dark') {
+        Appearance.setColorScheme('light');
+      } else {
+        Appearance.setColorScheme('dark');
+      }
+    } catch (error: any) {
+      throw new Error(error);
+    }
   };
 
   return (
@@ -28,7 +36,7 @@ export default function SettingsScreen() {
           <Ionicons name="brush" color={colorScheme === 'light' ? 'black' : 'white'} size={22} />
           <ThemedText>Dark mode</ThemedText>
         </View>
-        <CustomSwitch onValueChange={toggleSwitch} value={!isEnabled} isEnabled />
+        <CustomSwitch onValueChange={toggleSwitch} value={colorScheme === 'dark'} isEnabled />
       </View>
     </ThemedView>
   );
