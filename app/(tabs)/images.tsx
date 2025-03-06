d8c5bd2b79fddda097449be1b2d451270762ab8c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-03-06 19:39:41 +0700

    feat: update ImagesScreen to use themed components and integrate image generation button

--- app/(tabs)/images.tsx
+++ app/(tabs)/images.tsx
@@ -1,10 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { CustomButton } from '@/components/Button';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { generateImageFalAI } from '@/services/fal-ai';
+import { StyleSheet } from 'react-native';
+import * as FileSystem from 'expo-file-system';
 
 export default function ImagesScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Image</Text>
-    </View>
+    <ThemedView style={styles.container}>
+      <CustomButton onPress={() => generateImageFalAI()}>
+        <ThemedText>Press me</ThemedText>
+      </CustomButton>
+    </ThemedView>
   );
 }
 
