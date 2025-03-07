cf7a74991ad0ab56cb3ae2dbce98c5a8c7c50ba0
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-03-07 15:00:36 +0700

    refactor: rename FalaiContext to GenerateImageContext and update state management for generated images

--- context/GenerateImageContext.tsx
+++ context/GenerateImageContext.tsx
@@ -2,30 +2,38 @@ import { generateImageFalAI } from '@/services/fal-ai';
 import { FluxDevOutput } from '@fal-ai/client/endpoints';
 import { createContext, ReactNode, useState } from 'react';
 
-export const FalaiContext = createContext<{
+export const GenerateImageContext = createContext<{
   generateImageUsingAi: (input: string) => Promise<void>;
-  generatedImage: FluxDevOutput | undefined;
+  generatedImage: FalaiImageProps[] | undefined;
   loading: boolean;
+  input: string;
+  setInput: React.Dispatch<React.SetStateAction<string>>;
 }>({
   generateImageUsingAi: async () => {},
-  generatedImage: { timings: '', images: [], has_nsfw_concepts: [], prompt: '', seed: 0 },
+  generatedImage: [],
   loading: false,
+  input: '',
+  setInput: () => {},
 });
 
 type FalaiImageProps = FluxDevOutput & {
   input: string;
+  requestId: string;
 };
 
-export function FalaiProvider({ children }: { children: ReactNode }) {
-  const [generatedImage, setGeneratedImage] = useState<FalaiImageProps>({ input: '', images: [], timings: '', seed: 0, has_nsfw_concepts: [], prompt: '' });
+export function GenerateImageProvider({ children }: { children: ReactNode }) {
+  const [generatedImage, setGeneratedImage] = useState<FalaiImageProps[]>([]);
+
+  console.log(generatedImage);
+
   const [input, setInput] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
 
   const generateImageUsingAi = async () => {
     setLoading(true);
     try {
-      const { data } = await generateImageFalAI();
-      setGeneratedImage({ input: input, images: data.images, prompt: data.prompt, seed: data.seed, timings: data.timings, has_nsfw_concepts: data.has_nsfw_concepts });
+      const { data, requestId } = await generateImageFalAI(input);
+      setGeneratedImage((prev) => [...prev, { input: input, images: data.images, prompt: data.prompt, seed: data.seed, timings: data.timings, has_nsfw_concepts: data.has_nsfw_concepts, requestId }]);
       setLoading(false);
     } catch (error) {
       throw new Error(error as any);
@@ -34,5 +42,5 @@ export function FalaiProvider({ children }: { children: ReactNode }) {
     }
   };
 
-  return <FalaiContext.Provider value={{ generateImageUsingAi, generatedImage, loading }}>{children}</FalaiContext.Provider>;
+  return <GenerateImageContext.Provider value={{ generateImageUsingAi, generatedImage, loading, input, setInput }}>{children}</GenerateImageContext.Provider>;
 }
