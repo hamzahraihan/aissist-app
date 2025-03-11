899976482b71a9d6e39d0a20037ecbed0b2947fa
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-03-11 14:31:43 +0700

    fix: update prompt text in Cloudflare image generator function

--- services/cloudflare.ts
+++ services/cloudflare.ts
@@ -1,14 +1,14 @@
 import { Cloudflare } from 'cloudflare';
-import { AIRunResponse } from 'cloudflare/resources/ai/ai';
 
 const client = new Cloudflare({
   apiKey: process.env.EXPO_PUBLIC_CLOUDFLARE_API_KEY,
 });
+
 export const cloudflareImageGenerator = async (prompt: string) => {
   try {
-    const response: AIRunResponse = await client.ai.run('@cf/runwayml/stable-diffusion-v1-5-img2img', {
+    const response = await client.ai.run('@cf/runwayml/stable-diffusion-v1-5-img2img', {
       account_id: '9b37eea8034fbf61191d273e000f450e',
-      text: prompt,
+      text: 'create a dnd character',
     });
     return response;
   } catch (error) {
