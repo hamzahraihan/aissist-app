e3ac8236ece035bacddcf1e9075752f9232b4453
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-03-12 15:33:32 +0700

    add more ai models

--- constants/ai-model.ts
+++ constants/ai-model.ts
@@ -1,1 +1,26 @@
-export const AiModel = [{ model: 'falai' }, { model: 'openai' }, { model: 'stable-diff' }, { model: 'cloudflare' }];
+export const AiModels = [
+  {
+    name: 'flux-1-schnell',
+    model: '@cf/black-forest-labs/flux-1-schnell',
+  },
+  {
+    name: 'dreamshaper-8-lcm',
+    model: '@cf/lykon/dreamshaper-8-lcm',
+  },
+  {
+    name: 'stable-diffusion-v1-5-img2img',
+    model: '@cf/runwayml/stable-diffusion-v1-5-img2img',
+  },
+  {
+    name: 'stable-diffusion-v1-5-inpainting',
+    model: '@cf/runwayml/stable-diffusion-v1-5-inpainting',
+  },
+  {
+    name: 'stable-diffusion-xl-base-1.0',
+    model: '@cf/stabilityai/stable-diffusion-xl-base-1.0',
+  },
+  {
+    name: 'stable-diffusion-xl-lightning',
+    model: '@cf/bytedance/stable-diffusion-xl-lightning',
+  },
+];
