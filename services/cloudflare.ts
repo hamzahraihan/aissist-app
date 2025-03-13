a1950ff833f924f20b3b25513ad4422b49dd2514
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-03-13 19:00:02 +0700

    remove api key in cloudflare SDK

--- services/cloudflare.ts
+++ services/cloudflare.ts
@@ -1,18 +1,20 @@
 import { Cloudflare } from 'cloudflare';
 
 const client = new Cloudflare({
-  apiKey: process.env.EXPO_PUBLIC_CLOUDFLARE_API_KEY,
   apiToken: process.env.EXPO_PUBLIC_CLOUDFLARE_API_KEY,
 });
 
 export const cloudflareImageGenerator = async (prompt: string, modelName: string) => {
   try {
-    const response = await client.ai.run(modelName, {
+    console.log(`generating image with ${modelName}`);
+    const response: any = await client.ai.run(modelName, {
       account_id: '9b37eea8034fbf61191d273e000f450e',
       prompt: prompt,
     });
+
     return response;
-  } catch (error) {
-    console.error(error as any);
+  } catch (error: any) {
+    console.error(error.message);
+    throw new Error(error.message);
   }
 };
