4e69b6401ffb6403acadae4370a1fd4d6c8c01d9
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-03-14 09:37:40 +0700

    change ai-model file name to ai-image-model

---
+++ constants/ai-image-model.ts
@@ -0,0 +1,32 @@
+export const AiImageModels = [
+  {
+    name: 'flux-1-schnell',
+    model: '@cf/black-forest-labs/flux-1-schnell',
+    available: true,
+  },
+  {
+    name: 'dreamshaper-8-lcm',
+    model: '@cf/lykon/dreamshaper-8-lcm',
+    available: false,
+  },
+  {
+    name: 'stable-diffusion-v1-5-img2img',
+    model: '@cf/runwayml/stable-diffusion-v1-5-img2img',
+    available: false,
+  },
+  {
+    name: 'stable-diffusion-v1-5-inpainting',
+    model: '@cf/runwayml/stable-diffusion-v1-5-inpainting',
+    available: false,
+  },
+  {
+    name: 'stable-diffusion-xl-base-1.0',
+    model: '@cf/stabilityai/stable-diffusion-xl-base-1.0',
+    available: false,
+  },
+  {
+    name: 'stable-diffusion-xl-lightning',
+    model: '@cf/bytedance/stable-diffusion-xl-lightning',
+    available: false,
+  },
+];
