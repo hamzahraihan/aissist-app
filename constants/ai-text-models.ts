c34f350d49dcc1bb23cdf9df95ba21449fa4dade
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-03-14 14:48:16 +0700

    add a new models to use for text generator

---
+++ constants/ai-text-models.ts
@@ -0,0 +1,32 @@
+export const AiTextModels = [
+  {
+    name: 'deepseek-r1-distill-qwen-32b',
+    model: '@cf/deepseek-ai/deepseek-r1-distill-qwen-32b',
+    available: true,
+  },
+  {
+    name: 'deepseek-coder-6.7b-base-awq',
+    model: '@hf/thebloke/deepseek-coder-6.7b-base-awq',
+    available: true,
+  },
+  {
+    name: 'deepseek-math-7b-instruct',
+    model: '@cf/deepseek-ai/deepseek-math-7b-instruct',
+    available: true,
+  },
+  {
+    name: 'stable-diffusion-v1-5-inpainting',
+    model: '@cf/runwayml/stable-diffusion-v1-5-inpainting',
+    available: true,
+  },
+  {
+    name: 'stable-diffusion-xl-base-1.0',
+    model: '@cf/stabilityai/stable-diffusion-xl-base-1.0',
+    available: true,
+  },
+  {
+    name: 'stable-diffusion-xl-lightning',
+    model: '@cf/bytedance/stable-diffusion-xl-lightning',
+    available: true,
+  },
+];
