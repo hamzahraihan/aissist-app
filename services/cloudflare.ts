6aa434c4022390d7a845e64d014e5624043e0651
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-03-14 09:41:59 +0700

    feat: add cloudflareTextGenerator function for AI text generation

--- services/cloudflare.ts
+++ services/cloudflare.ts
@@ -1,4 +1,5 @@
 import { Cloudflare } from 'cloudflare';
+import { AIRunParams } from 'cloudflare/resources/ai/ai';
 
 const client = new Cloudflare({
   apiToken: process.env.EXPO_PUBLIC_CLOUDFLARE_API_KEY,
@@ -15,6 +16,20 @@ export const cloudflareImageGenerator = async (prompt: string, modelName: string
     return response;
   } catch (error: any) {
     console.error(error.message);
+    throw new Error(error.message);
+  }
+};
+
+export const cloudflareTextGenerator = async (prompt: AIRunParams.Messages.Message[], modelName: string) => {
+  try {
+    console.log(`generating text with ${modelName}`);
+
+    const stream = await client.ai.run(modelName, {
+      account_id: '9b37eea8034fbf61191d273e000f450e',
+      stream: true,
+      messages: prompt,
+    });
+  } catch (error: any) {
     throw new Error(error.message);
   }
 };
