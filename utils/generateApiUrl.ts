f365d639ec703d09bc640af214d82883388839e0
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-03-16 19:28:54 +0700

    fix: handle undefined relativePath in generateAPIUrl and improve URL generation logic

--- utils/generateApiUrl.ts
+++ utils/generateApiUrl.ts
@@ -1,12 +1,13 @@
 import Constants from 'expo-constants';
 
-export const generateAPIUrl = (relativePath: string) => {
-  const origin = Constants.experienceUrl.replace('exp://', 'http://');
+export const generateAPIUrl = (relativePath: string | undefined) => {
+  if (!relativePath) return;
+  const origin = Constants?.experienceUrl?.replace('exp://', 'http://') || process.env.EXPO_PUBLIC_API_BASE_URL;
 
-  const path = relativePath.startsWith('/') ? relativePath : `/${relativePath}`;
+  const path = relativePath?.startsWith('/') ? relativePath : `/${relativePath}`;
 
   if (process.env.NODE_ENV === 'development') {
-    return origin.concat(path);
+    return origin?.concat(path);
   }
 
   if (!process.env.EXPO_PUBLIC_API_BASE_URL) {
