c01fcdc3ed4edca5ab064ef7c6cb0dbc963043b4
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-03-16 19:28:24 +0700

    refactor chat API to use fetch for external requests and handle streaming response

--- app/api/chat+api.ts
+++ app/api/chat+api.ts
@@ -1,31 +1,52 @@
-import { createWorkersAI } from 'workers-ai-provider';
-import { streamText } from 'ai';
-import { AI } from 'cloudflare/resources/radar/ai/ai';
+import { fetch } from 'expo/fetch';
+import { generateAPIUrl } from '@/utils/generateApiUrl';
 
 type Env = {
-  AI: AI;
+  AI: Ai;
 };
 
-export async function POST(req: Request, env: Env) {
-  const body = await req.json();
-  console.log(body);
-
+export default async function POST(req: Request, env: Env) {
+  const body: { model: any; prompt: string } = await req.json();
   const { model, prompt } = body;
-  const workersai = createWorkersAI({ binding: env.AI });
 
-  const result = streamText({
-    model: workersai(model),
-    prompt,
-  });
-  console.log(result);
+  if (!prompt) {
+    return new Response('No prompt provided', { status: 400 });
+  }
 
-  return result.toTextStreamResponse({
-    // add these headers to ensure that the
-    // response is chunked and streamed
-    headers: {
-      'Content-Type': 'text/x-unknown',
-      'content-encoding': 'identity',
-      'transfer-encoding': 'chunked',
-    },
-  });
+  try {
+    const response = await fetch(generateAPIUrl(process.env.EXPO_PUBLIC_API_BASE_URL) || '', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ model, prompt }),
+    });
+
+    if (response.body) {
+      const reader = response.body.getReader();
+      const textDecoder = new TextDecoder();
+      let accumulatedText = '';
+
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+
+        const chunk = textDecoder.decode(value, { stream: true });
+        accumulatedText += chunk;
+
+        console.log('Received chunk', chunk);
+      }
+      console.log('Final completed text', accumulatedText);
+      return new Response(accumulatedText, {
+        headers: {
+          'Content-Type': 'text/x-unknown',
+          'content-encoding': 'identity',
+          'transfer-encoding': 'chunked',
+        },
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    return new Response('Error', { status: 500 });
+  }
 }
