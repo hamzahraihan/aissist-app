2567c6c7e722391a8b4718c0a69f92ed19bcb0c2
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-03-22 15:11:40 +0700

    feat: implement POST function in generateImage API for image generation

--- app/api/generateImage+api.ts
+++ app/api/generateImage+api.ts
@@ -1,1 +1,26 @@
-export async function POST() {}
+import { Cloudflare } from 'cloudflare';
+
+const client = new Cloudflare({
+  apiToken: process.env.EXPO_PUBLIC_CLOUDFLARE_API_KEY,
+});
+
+export async function POST(req: Request) {
+  const { prompt, modelName }: { prompt: string; modelName: string } = await req.json();
+
+  if (!prompt) {
+    return new Response('No prompt provided', { status: 400 });
+  }
+
+  try {
+    console.log(`generating image with ${modelName}`);
+    const response: any = await client.ai.run(modelName, {
+      account_id: '9b37eea8034fbf61191d273e000f450e',
+      prompt: prompt,
+    });
+
+    return Response.json(response);
+  } catch (error) {
+    console.error(error);
+    return new Response('Error', { status: 500 });
+  }
+}
