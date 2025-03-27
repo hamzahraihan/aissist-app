0ddee67093c17737efc55866d93956bd534996e2
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-03-27 20:41:46 +0700

    feat: enhance HomeScreen with TikTok card and dynamic icon colors based on theme

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
 import { Card } from '@/components/Card';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { StyleSheet, View } from 'react-native';
+import { useCustomTheme } from '@/context/ThemeContext';
+import { Ionicons } from '@expo/vector-icons';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
 export default function HomeScreen() {
+  const { themeMode } = useCustomTheme();
+  const iconColor = themeMode === 'dark' ? 'white' : 'black';
   return (
     <ThemedView style={{ flex: 1 }}>
       <ScrollView style={styles.container}>
@@ -14,13 +18,23 @@ export default function HomeScreen() {
           </ThemedText>
 
           <ScrollView horizontal contentContainerStyle={{ gap: 20, paddingHorizontal: 34 }} showsHorizontalScrollIndicator={false}>
+            <TouchableOpacity>
+              <Card style={styles.card}>
+                <Ionicons name="logo-tiktok" size={32} color={iconColor} />
+                <ThemedText style={{ color: 'gray', fontSize: 12, textAlign: 'center' }} type="default">
+                  Generate a content for TikTok
+                </ThemedText>
+                <View>
+                  <Ionicons name="chevron-forward-outline" size={32} color={iconColor} />
+                </View>
+              </Card>
+            </TouchableOpacity>
             <Card style={styles.card}>
-              <ThemedText type="default">Hello</ThemedText>
-            </Card>
-            <Card style={styles.card}>
+              <Ionicons name="logo-instagram" size={32} color={iconColor} />
               <ThemedText>Hello</ThemedText>
             </Card>
             <Card style={styles.card}>
+              <Ionicons name="logo-facebook" size={32} color={iconColor} />
               <ThemedText>Hello</ThemedText>
             </Card>
           </ScrollView>
@@ -73,8 +87,9 @@ const styles = StyleSheet.create({
   card: {
     display: 'flex',
     gap: 10,
+    padding: 12,
     height: 163,
-    width: 129,
+    width: 130,
     justifyContent: 'center',
     alignItems: 'center',
   },
