c86be55c36445820c1557de77c536330138be455
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-04-01 19:52:05 +0700

    feat: enhance ModelSelector to display model version with styling

--- components/ui/ModelSelector.tsx
+++ components/ui/ModelSelector.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-unresolved */
 import React, { useCallback } from 'react';
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 import CustomBottomSheet from '../BottomSheet';
 import { ThemedView } from '../ThemedView';
 import { CustomText } from '../Text';
@@ -24,10 +24,18 @@ export default function ModelSelector({ models, selectedModel, onModelSelect, bo
     [onModelSelect, bottomSheetModalRef]
   );
 
+  const handleVersionColor = (version: string) => {
+    if (version === 'beta') return { backgroundColor: '#ffd9d9', borderColor: '#ff5555', color: '#ff5555' };
+    return { backgroundColor: '#cac4ff', borderColor: 'blue', color: 'blue' };
+  };
+
   const renderModelItem = useCallback(
     (item: AiModelType) => (
       <TouchableOpacity disabled={!item.available} key={item.name} onPress={() => handleSelectModel(item.model)}>
-        <ThemedView onSelected={item.model === selectedModel} style={[styles.sheetSelectableContent, { backgroundColor: item.model === selectedModel ? '#272727' : '' }]}>
+        <ThemedView
+          onSelected={item.model === selectedModel}
+          style={[styles.sheetSelectableContent, { display: 'flex', flexDirection: 'row', gap: 8, justifyContent: 'center', alignItems: 'center', backgroundColor: item.model === selectedModel ? '#272727' : '' }]}
+        >
           <CustomText
             style={{
               color: item.model === selectedModel ? 'white' : handleModelAvailable(item.available),
@@ -36,6 +44,19 @@ export default function ModelSelector({ models, selectedModel, onModelSelect, bo
           >
             {item.name}
           </CustomText>
+          <Text
+            style={{
+              backgroundColor: handleVersionColor(item.version as string).backgroundColor,
+              color: handleVersionColor(item.version as string).color,
+              paddingVertical: 4,
+              paddingHorizontal: 8,
+              borderRadius: 12,
+              borderColor: handleVersionColor(item.version as string).borderColor,
+              borderWidth: 2,
+            }}
+          >
+            {item.version}
+          </Text>
         </ThemedView>
       </TouchableOpacity>
     ),
