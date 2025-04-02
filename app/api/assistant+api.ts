24322be82d48021cfea80667acbd9661b40c6e36
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-04-02 12:10:28 +0700

    feat: update socialMediaSchema and modify POST function for improved content generation

--- app/api/assistant+api.ts
+++ app/api/assistant+api.ts
@@ -5,22 +5,24 @@ import { z } from 'zod';
 export const socialMediaSchema = z.object({
   title: z.string(),
   content: z.string(),
-  thoughts: z.string(),
+  thoughts: z.string().describe('Strategy thoughts on why this content would perform well'),
 });
 
 export async function POST(req: Request) {
-  const { category, prompt, description }: any = await req.json();
-
+  // const { category, prompt, description }: any = await req.json();
+  const context = await req.json();
+  console.log(context);
   try {
     const result = streamObject({
       model: openai('gpt-4o-mini', { structuredOutputs: true }),
-      schemaDescription: description,
-      schemaName: category,
+      schemaDescription: 'you are the best content create of tiktok. you will help people to create the best content to get so much views.',
+      schemaName: 'tiktok',
       schema: socialMediaSchema,
-      prompt: `Here is the post idea: ${prompt}`,
+      prompt: `Here is the post idea: ${context}`,
     });
 
     return result.toTextStreamResponse();
+    // return result.toTextStreamResponse({ headers: { 'Content-Type': 'text/plain; charset=utf-8' } });
   } catch (error) {
     console.error(error);
     return new Response('Error', { status: 500 });
