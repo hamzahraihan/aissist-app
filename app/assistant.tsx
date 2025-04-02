f23190d8eff7eb749d9394fa85b5c4895a757882
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-04-02 12:10:10 +0700

    feat: integrate useGenerateAssistant hook and add generate button in GeneratedContentModal

--- app/assistant.tsx
+++ app/assistant.tsx
@@ -1,9 +1,13 @@
 /* eslint-disable import/no-unresolved */
 import { CustomText } from '@/components/Text';
 import { ThemedView } from '@/components/ThemedView';
-import { StyleSheet, View } from 'react-native';
+import { useGenerateAssistant } from '@/hooks/useGenerateAssistant';
+import { Button, StyleSheet, View } from 'react-native';
 
 export default function GeneratedContentModal() {
+  const { object, isLoading, error, submit } = useGenerateAssistant();
+  console.log('generated object: ', object);
+  console.log(error);
   return (
     <ThemedView style={styles.container}>
       <View style={{ display: 'flex', gap: 12 }}>
@@ -11,6 +15,7 @@ export default function GeneratedContentModal() {
         <ThemedView type="assistant" style={{ paddingHorizontal: 24, paddingVertical: 12, borderRadius: 12 }}>
           <CustomText type="subtitle">Content Title</CustomText>
         </ThemedView>
+        <Button title="Generate" disabled={isLoading} onPress={() => submit('i want to create a popular content that will get more views, content will be like a meme and adding a valueable knowledge')} />
       </View>
     </ThemedView>
   );
