aef829bace21bb65d7250f3cab0d2860f209fb6c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-04-02 12:10:37 +0700

    feat: enhance GenerateAssistantProvider with error handling and custom fetch implementation

--- context/GenerateAssistantContext.tsx
+++ context/GenerateAssistantContext.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable import/no-unresolved */
 import { experimental_useObject as useObject } from '@ai-sdk/react';
-import { generateAPIUrl } from '@/utils/generateApiUrl';
 import { createContext, Dispatch, ReactNode, SetStateAction, useState } from 'react';
 import { socialMediaSchema } from '@/app/api/assistant+api';
+import { fetch as expoFetch } from 'expo/fetch';
+import { generateAPIUrl } from '@/utils/generateApiUrl';
 
 export const GenerateAssistantContext = createContext<{
   object: any;
@@ -25,7 +26,12 @@ export const GenerateAssistantContext = createContext<{
 });
 
 export const GenerateAssistantProvider = ({ children }: { children: ReactNode }) => {
-  const { object, error, submit, isLoading } = useObject({ api: generateAPIUrl('/api/assistant') as string, schema: socialMediaSchema });
+  const { object, error, submit, isLoading } = useObject({
+    onError: (error) => console.error(error),
+    fetch: expoFetch as unknown as typeof globalThis.fetch,
+    api: generateAPIUrl('/api/assistant') as string,
+    schema: socialMediaSchema,
+  });
 
   const [input, setInput] = useState<string>('');
 
