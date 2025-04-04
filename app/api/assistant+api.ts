892e449bceb78e88c4214321794d9ac3f7ac66fc
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-04-04 20:26:15 +0700

    feat: update POST function in assistant+api.ts to utilize dynamic schema handling and improve input structure

--- app/api/assistant+api.ts
+++ app/api/assistant+api.ts
@@ -1,23 +1,18 @@
+import { handleAiSchema } from '@/constants/assistants';
 import { openai } from '@ai-sdk/openai';
 import { streamObject } from 'ai';
-import { z } from 'zod';
-
-export const socialMediaSchema = z.object({
-  title: z.string().describe('Provide a concise and engaging title for the social media post.'),
-  content: z.string().describe('Write the main content of the post, ensuring it is relevant and captivating.'),
-  thoughts: z.string().describe('Provide a detailed strategy explaining why this content would perform well, ensuring consistency and accuracy across generations.'),
-});
 
 export async function POST(req: Request) {
-  const context = await req.json();
-  console.log(context);
+  const { inputPrompt, schemaName, schemaDescription }: any = await req.json();
+  console.log(inputPrompt);
+
   try {
     const result = streamObject({
       model: openai('gpt-4o-mini', { structuredOutputs: true }),
-      schemaDescription: 'you are the best content create of tiktok. you will help people to create the best content to get so much views.',
-      schemaName: 'tiktok',
-      schema: socialMediaSchema,
-      prompt: `Here is the post idea: ${context}`,
+      schemaDescription,
+      schemaName,
+      schema: handleAiSchema(schemaName),
+      prompt: `Here is the post idea: ${inputPrompt}`,
     });
 
     return result.toTextStreamResponse();
