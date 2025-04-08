cecdef183257561d0df25cb9ec147925a8dbbc78
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-04-08 10:43:11 +0700

    feat: add SocialMediaAssistantResponse component for generating TikTok content ideas

---
+++ components/ui/AsssitantResponse.tsx
@@ -0,0 +1,50 @@
+/* eslint-disable import/no-unresolved */
+import { AIResponse } from '@/components/AIResponse';
+import { CustomText } from '@/components/Text';
+import { CustomTextInput } from '@/components/TextInput';
+import { ThemedView } from '@/components/ThemedView';
+import { Button, TouchableOpacity, View } from 'react-native';
+
+export function SocialMediaAssistantResponse({ object, isLoading, submit }: any) {
+  return (
+    <>
+      <View style={{ gap: 14 }}>
+        <CustomTextInput style={{ borderWidth: 2, borderRadius: 12, paddingHorizontal: 16, paddingVertical: 8 }} placeholder="Generate a Tiktok content Idea ✨" />
+        <TouchableOpacity style={{ padding: 8, backgroundColor: '#fcfcfc', borderRadius: 12, overflow: 'hidden' }}>
+          <CustomText style={{ color: 'black', textAlign: 'center' }}>Generate</CustomText>
+        </TouchableOpacity>
+        <Button title="Generate" color={'gray'} disabled={isLoading} onPress={() => submit({ initialPrompt: '' })} />
+      </View>
+
+      {/* divider line */}
+      <View style={{ marginVertical: 14, marginHorizontal: 10, flex: 1, height: 1, borderRadius: 99, backgroundColor: '#5e5e5e' }} />
+
+      <View style={{ display: 'flex', gap: 14 }}>
+        <View style={{ gap: 8 }}>
+          <CustomText style={{ paddingHorizontal: 24 }}>Recommended Content Title</CustomText>
+          <ThemedView type="assistant" style={{ paddingHorizontal: 24, borderRadius: 12 }}>
+            {object !== undefined ? <AIResponse>{object.title}</AIResponse> : <CustomText style={{ paddingVertical: 6 }}>Content Title</CustomText>}
+          </ThemedView>
+        </View>
+
+        <View style={{ gap: 8 }}>
+          <CustomText style={{ paddingHorizontal: 24 }}>Recommended Content Description</CustomText>
+          <ThemedView type="assistant" style={{ paddingHorizontal: 24, borderRadius: 12 }}>
+            <AIResponse>{object !== undefined ? object.content : 'content description'}</AIResponse>
+          </ThemedView>
+        </View>
+
+        <View style={{ paddingBottom: 80, gap: 8 }}>
+          <CustomText style={{ paddingHorizontal: 24 }}>Thoughts on this content</CustomText>
+          <ThemedView type="assistant" style={{ paddingHorizontal: 24, borderRadius: 12 }}>
+            <AIResponse>{object !== undefined ? object.thoughts : 'content description'}</AIResponse>
+          </ThemedView>
+        </View>
+      </View>
+    </>
+  );
+}
+
+export function HealthAssistantResponse() {
+  return;
+}
