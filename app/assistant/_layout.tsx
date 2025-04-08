7b006334051ca0c57839de28fb3f3580ab541a8a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-04-08 18:25:17 +0700

    refactor: streamline AssistantLayout component for improved readability

--- app/assistant/_layout.tsx
+++ app/assistant/_layout.tsx
@@ -1,25 +1,11 @@
-/* eslint-disable import/no-unresolved */
-import { darkTheme, lightTheme } from '@/constants/theme';
-import { useCustomTheme } from '@/context/ThemeContext';
 import { Stack } from 'expo-router';
 
-export default function AssistantLayout() {
-  const { themeMode } = useCustomTheme();
+const AssistantLayout = () => {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen
-        name="/assistant/[assistantId]"
-        options={{
-          presentation: 'containedModal',
-          animation: 'fade_from_bottom',
-          headerShown: true,
-          headerShadowVisible: false,
-          headerTitle: 'AI Assistant',
-          headerStyle: {
-            backgroundColor: themeMode === 'light' ? lightTheme.backgroundColor : darkTheme.backgroundColor,
-          },
-        }}
-      />
+      <Stack.Screen name="[assistantId]" />
     </Stack>
   );
-}
+};
+
+export default AssistantLayout;
