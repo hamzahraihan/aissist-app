45218c8cf57b3f64852bf7100f5f2f996ae13348
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-04-11 14:54:04 +0700

    refactor: restructure AssistantFormView for improved clarity and maintainability

--- app/assistant/[assistantId].tsx
+++ app/assistant/[assistantId].tsx
@@ -1,17 +1,23 @@
 /* eslint-disable import/no-unresolved */
 import { ThemedView } from '@/components/ThemedView';
-import { SocialMediaAssistantResponse } from '@/components/ui/AsssitantResponse';
-import { AI_ASSISTANTS } from '@/constants/assistants';
+import { AssistantResponse } from '@/components/ui/AsssitantResponse';
+import { AI_ASSISTANTS, AiResponse } from '@/constants/assistants';
 import { useGenerateAssistant } from '@/hooks/useGenerateAssistant';
 import { useLocalSearchParams } from 'expo-router';
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+
+const AssistantFormView = ({ input, setInput, assistant, object, isLoading, submit }: { input: string; setInput: Dispatch<SetStateAction<string>>; assistant: AiResponse | undefined; object: any; isLoading: boolean; submit: any }) => {
+  const { assistantId, assistantType } = useLocalSearchParams();
+  console.log(`assistanttype: ${assistantType}`, assistantId);
+
+  return <AssistantResponse assistantType={assistantType} input={input} setInput={setInput} object={object} submit={submit} isLoading={isLoading} assistant={assistant} />;
+};
 
 export default function GeneratedContentModal() {
   const [input, setInput] = useState<string>('');
   const { assistantId, assistantType } = useLocalSearchParams();
-  console.log(`assistanttype: ${assistantType}`, assistantId);
 
   const { object, isLoading, error, submit } = useGenerateAssistant();
   console.log('generated object: ', object);
@@ -29,7 +35,9 @@ export default function GeneratedContentModal() {
 
   return (
     <ThemedView style={styles.container}>
-      <ScrollView style={{ flex: 1 }}>{assistantType === 'social' ? <SocialMediaAssistantResponse input={input} setInput={setInput} object={object} submit={submit} isLoading={isLoading} assistant={assistant} /> : null}</ScrollView>
+      <ScrollView style={{ flex: 1 }}>
+        <AssistantFormView input={input} assistant={assistant} isLoading={isLoading} object={object} setInput={setInput} submit={submit} />
+      </ScrollView>
     </ThemedView>
   );
 }
