a1340e7ee713a8de31aa79afca0e02bcb818be69
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-04-12 14:21:08 +0700

    refactor: clean up code structure and improve readability in GeneratedContentModal

--- app/assistant/[assistantId].tsx
+++ app/assistant/[assistantId].tsx
@@ -1,27 +1,15 @@
 /* eslint-disable import/no-unresolved */
 import { ThemedView } from '@/components/ThemedView';
 import { AssistantResponse } from '@/components/ui/AsssitantResponse';
-import { AI_ASSISTANTS, AiResponse } from '@/constants/assistants';
-import { useGenerateAssistant } from '@/hooks/useGenerateAssistant';
+import { AI_ASSISTANTS } from '@/constants/assistants';
 import { useLocalSearchParams } from 'expo-router';
-import { Dispatch, SetStateAction, useState } from 'react';
+import { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-
-const AssistantFormView = ({ input, setInput, assistant, object, isLoading, submit }: { input: string; setInput: Dispatch<SetStateAction<string>>; assistant: AiResponse | undefined; object: any; isLoading: boolean; submit: any }) => {
-  const { assistantId, assistantType } = useLocalSearchParams();
-  console.log(`assistanttype: ${assistantType}`, assistantId);
-
-  return <AssistantResponse assistantType={assistantType} input={input} setInput={setInput} object={object} submit={submit} isLoading={isLoading} assistant={assistant} />;
-};
 
 export default function GeneratedContentModal() {
   const [input, setInput] = useState<string>('');
   const { assistantId, assistantType } = useLocalSearchParams();
-
-  const { object, isLoading, error, submit } = useGenerateAssistant();
-  console.log('generated object: ', object);
-  console.log(error);
 
   const assistant = AI_ASSISTANTS.map((item) => {
     if (assistantType === 'social') {
@@ -36,7 +24,7 @@ export default function GeneratedContentModal() {
   return (
     <ThemedView style={styles.container}>
       <ScrollView style={{ flex: 1 }}>
-        <AssistantFormView input={input} assistant={assistant} isLoading={isLoading} object={object} setInput={setInput} submit={submit} />
+        <AssistantResponse input={input} setInput={setInput} />
       </ScrollView>
     </ThemedView>
   );
