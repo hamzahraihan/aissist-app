cf865b51a9613816976c774ab8e4874f5706abe7
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-04-12 14:21:25 +0700

    refactor: improve code structure and readability in AssistantResponse component

--- components/ui/AsssitantResponse.tsx
+++ components/ui/AsssitantResponse.tsx
@@ -3,28 +3,16 @@ import { AIResponse } from '@/components/AIResponse';
 import { CustomText } from '@/components/Text';
 import { CustomTextInput } from '@/components/TextInput';
 import { ThemedView } from '@/components/ThemedView';
-import { AiResponse } from '@/constants/assistants';
+import { AI_ASSISTANTS, AssistantResponsesTypes } from '@/constants/assistants';
 import { Dispatch, SetStateAction } from 'react';
 import { View } from 'react-native';
 import { CustomButton } from '../Button';
+import { useLocalSearchParams } from 'expo-router';
+import { useGenerateAssistant } from '@/hooks/useGenerateAssistant';
 
-export function AssistantResponse({
-  input,
-  setInput,
-  assistant,
-  object,
-  isLoading,
-  submit,
-  assistantType,
-}: {
-  assistant: AiResponse | undefined;
-  object: any;
-  isLoading: boolean;
-  submit: any;
-  input: string;
-  setInput: Dispatch<SetStateAction<string>>;
-  assistantType: string | string[];
-}) {
+function AssistantForm({ assistant }: { assistant: AssistantResponsesTypes | undefined }) {
+  const { assistantType } = useLocalSearchParams();
+  const { object } = useGenerateAssistant();
   const responseSchema = () => {
     if (assistantType === 'social') {
       return {
@@ -39,7 +27,7 @@ export function AssistantResponse({
     }
     if (assistantType === 'health') {
       return {
-        inputPlaceholder: 'Describe your age, gender, symptoms, allergies, and your current medications for your disease.',
+        inputPlaceholder: assistant?.placeholder,
         agePlaceholder: 'Age',
         genderPlaceholder: 'Gender',
         symptomsPlaceholder: 'Symptoms',
@@ -57,6 +45,58 @@ export function AssistantResponse({
   };
 
   const response = responseSchema();
+
+  if (assistantType === 'social') {
+    return (
+      <>
+        <View style={{ gap: 8 }}>
+          <CustomText style={{ paddingHorizontal: 24 }}>Content Title</CustomText>
+          <ThemedView type="assistant" style={{ paddingHorizontal: 24, borderRadius: 12 }}>
+            <AIResponse>{response?.title === undefined ? response?.titlePlaceholer : response.content}</AIResponse>
+          </ThemedView>
+        </View>
+
+        <View style={{ gap: 8 }}>
+          <CustomText style={{ paddingHorizontal: 24 }}>Content Description</CustomText>
+          <ThemedView type="assistant" style={{ paddingHorizontal: 24, borderRadius: 12 }}>
+            <AIResponse>{object !== undefined ? object.content : response?.contentPlaceholder}</AIResponse>
+          </ThemedView>
+        </View>
+
+        <View style={{ paddingBottom: 80, gap: 8 }}>
+          <CustomText style={{ paddingHorizontal: 24 }}>Thoughts on this content</CustomText>
+          <ThemedView type="assistant" style={{ paddingHorizontal: 24, borderRadius: 12 }}>
+            <AIResponse>{object !== undefined ? object.thoughts : 'Thoughts About Content'}</AIResponse>
+          </ThemedView>
+        </View>
+      </>
+    );
+  }
+}
+
+export function AssistantResponse({ input, setInput }: { input: string; setInput: Dispatch<SetStateAction<string>> }) {
+  const { assistantId, assistantType } = useLocalSearchParams();
+  const { isLoading, submit } = useGenerateAssistant();
+  const assistant = AI_ASSISTANTS.map((item) => {
+    if (assistantType === 'social') return item.socialMedia.filter((item) => item.type === assistantId)[0];
+    if (assistantType === 'health') return item.health.filter((item) => item.type === assistantId)[0];
+  })[0];
+
+  const responseSchema = () => {
+    if (assistantType === 'social') {
+      return {
+        inputPlaceholder: assistant?.placeholder,
+      };
+    }
+    if (assistantType === 'health') {
+      return {
+        inputPlaceholder: assistant?.placeholder,
+      };
+    }
+  };
+
+  const response = responseSchema();
+
   return (
     <>
       <View style={{ gap: 14 }}>
@@ -74,83 +114,7 @@ export function AssistantResponse({
       <View style={{ marginVertical: 14, marginHorizontal: 10, flex: 1, height: 1, borderRadius: 99, backgroundColor: '#5e5e5e' }} />
 
       <View style={{ display: 'flex', gap: 14 }}>
-        <View style={{ gap: 8 }}>
-          <CustomText style={{ paddingHorizontal: 24 }}>{response!.titlePlaceholer}</CustomText>
-          <ThemedView type="assistant" style={{ paddingHorizontal: 24, borderRadius: 12 }}>
-            <AIResponse>{response?.title === undefined ? response?.titlePlaceholer : response.content}</AIResponse>
-          </ThemedView>
-        </View>
-
-        <View style={{ gap: 8 }}>
-          <CustomText style={{ paddingHorizontal: 24 }}>Recommended Content Description</CustomText>
-          <ThemedView type="assistant" style={{ paddingHorizontal: 24, borderRadius: 12 }}>
-            <AIResponse>{object !== undefined ? object.content : 'Content Description'}</AIResponse>
-          </ThemedView>
-        </View>
-
-        <View style={{ paddingBottom: 80, gap: 8 }}>
-          <CustomText style={{ paddingHorizontal: 24 }}>Thoughts on this content</CustomText>
-          <ThemedView type="assistant" style={{ paddingHorizontal: 24, borderRadius: 12 }}>
-            <AIResponse>{object !== undefined ? object.thoughts : 'Thoughts About Content'}</AIResponse>
-          </ThemedView>
-        </View>
-      </View>
-    </>
-  );
-}
-
-export function HealthAssistantResponse({
-  input,
-  setInput,
-  assistant,
-  object,
-  isLoading,
-  submit,
-}: {
-  assistant: AiResponse | undefined;
-  object: any;
-  isLoading: boolean;
-  submit: any;
-  input: string;
-  setInput: Dispatch<SetStateAction<string>>;
-}) {
-  return (
-    <>
-      <View style={{ gap: 14 }}>
-        <CustomTextInput style={{ borderWidth: 2, borderRadius: 12, paddingHorizontal: 16, paddingVertical: 8 }} placeholder={assistant?.placeholder} onChangeText={setInput} value={input} />
-        <CustomButton
-          disabled={isLoading}
-          style={{ padding: 8, borderRadius: 12, overflow: 'hidden' }}
-          onPress={() => submit({ initialPrompt: assistant?.initialPrompt, prompt: input, schemaName: assistant?.type, description: assistant?.description })}
-        >
-          <CustomText style={{ textAlign: 'center' }}>Generate</CustomText>
-        </CustomButton>
-      </View>
-
-      {/* divider line */}
-      <View style={{ marginVertical: 14, marginHorizontal: 10, flex: 1, height: 1, borderRadius: 99, backgroundColor: '#5e5e5e' }} />
-
-      <View style={{ display: 'flex', gap: 14 }}>
-        <View style={{ gap: 8 }}>
-          <CustomText style={{ paddingHorizontal: 24 }}>Recommended Content Title</CustomText>
-          <ThemedView type="assistant" style={{ paddingHorizontal: 24, borderRadius: 12 }}>
-            <AIResponse>{object !== undefined ? object?.title : 'Content Title'}</AIResponse>
-          </ThemedView>
-        </View>
-
-        <View style={{ gap: 8 }}>
-          <CustomText style={{ paddingHorizontal: 24 }}>Recommended Content Description</CustomText>
-          <ThemedView type="assistant" style={{ paddingHorizontal: 24, borderRadius: 12 }}>
-            <AIResponse>{object !== undefined ? object.content : 'Content Description'}</AIResponse>
-          </ThemedView>
-        </View>
-
-        <View style={{ paddingBottom: 80, gap: 8 }}>
-          <CustomText style={{ paddingHorizontal: 24 }}>Thoughts on this content</CustomText>
-          <ThemedView type="assistant" style={{ paddingHorizontal: 24, borderRadius: 12 }}>
-            <AIResponse>{object !== undefined ? object.thoughts : 'Thoughts About Content'}</AIResponse>
-          </ThemedView>
-        </View>
+        <AssistantForm assistant={assistant} />
       </View>
     </>
   );
