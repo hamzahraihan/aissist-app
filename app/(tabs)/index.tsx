037e6aba0218963fdb9f0a2c3ef9a533c174c57f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-04-16 08:54:44 +0700

    refactor HomeScreen to use HorizontalCardList component for improved code reusability

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,15 +6,32 @@ import { Ionicons } from '@expo/vector-icons';
 import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useRouter } from 'expo-router';
-import { AI_ASSISTANTS } from '@/constants/assistants';
+import { AI_ASSISTANTS, AssistantResponsesTypes } from '@/constants/assistants';
+import { memo } from 'react';
 
-export default function HomeScreen() {
+const HorizontalCardList = memo(function HorizontalCardList({ item }: { item: AssistantResponsesTypes }) {
   const { themeMode } = useCustomTheme();
+
+  const router = useRouter();
   const iconColor = themeMode === 'dark' ? 'white' : 'black';
   const backgroundIconColor = themeMode === 'dark' ? '#464646' : '#e2e2e2';
 
-  const router = useRouter();
+  return (
+    <TouchableOpacity onPress={() => router.navigate(`/assistant/${item.type}?assistantType=${item.assistantType}`)}>
+      <Card style={styles.card}>
+        <Ionicons name={item.logo as any} size={24} color={iconColor} />
+        <CustomText style={{ color: 'gray', fontSize: 10, textAlign: 'center' }} type="default">
+          {item.subtitle}
+        </CustomText>
+        <View style={{ padding: 10, backgroundColor: backgroundIconColor, borderRadius: 99 }}>
+          <Ionicons name="chevron-forward-outline" size={18} color={iconColor} />
+        </View>
+      </Card>
+    </TouchableOpacity>
+  );
+});
 
+export default function HomeScreen() {
   return (
     <ThemedView style={{ flex: 1 }}>
       <ScrollView style={styles.container}>
@@ -24,21 +41,7 @@ export default function HomeScreen() {
           </CustomText>
 
           <ScrollView horizontal contentContainerStyle={{ gap: 20, paddingHorizontal: 34 }} showsHorizontalScrollIndicator={false}>
-            {AI_ASSISTANTS.map((item) =>
-              item.socialMedia.map((social) => (
-                <TouchableOpacity onPress={() => router.navigate(`/assistant/${social.type}?assistantType=${social.assistantType}`)}>
-                  <Card style={styles.card}>
-                    <Ionicons name={social.logo as any} size={24} color={iconColor} />
-                    <CustomText style={{ color: 'gray', fontSize: 10, textAlign: 'center' }} type="default">
-                      {social.placeholder}
-                    </CustomText>
-                    <View style={{ padding: 10, backgroundColor: backgroundIconColor, borderRadius: 99 }}>
-                      <Ionicons name="chevron-forward-outline" size={18} color={iconColor} />
-                    </View>
-                  </Card>
-                </TouchableOpacity>
-              ))
-            )}
+            {AI_ASSISTANTS.map((item) => item.socialMedia.map((social) => <HorizontalCardList item={social} />))}
           </ScrollView>
         </View>
 
@@ -48,21 +51,7 @@ export default function HomeScreen() {
           </CustomText>
 
           <ScrollView horizontal contentContainerStyle={{ gap: 20, paddingHorizontal: 34 }} showsHorizontalScrollIndicator={false}>
-            {AI_ASSISTANTS.map((item) =>
-              item.health.map((health) => (
-                <TouchableOpacity onPress={() => router.navigate(`/assistant/${health.type}?assistantType=${health.assistantType}`)}>
-                  <Card style={styles.card}>
-                    <Ionicons name="medical" size={24} color={iconColor} />
-                    <CustomText style={{ color: 'gray', fontSize: 10, textAlign: 'center' }} type="default">
-                      Generate a Medical Recommendation
-                    </CustomText>
-                    <View style={{ padding: 10, backgroundColor: backgroundIconColor, borderRadius: 99 }}>
-                      <Ionicons name="chevron-forward-outline" size={18} color={iconColor} />
-                    </View>
-                  </Card>
-                </TouchableOpacity>
-              ))
-            )}
+            {AI_ASSISTANTS.map((item) => item.health.map((health) => <HorizontalCardList item={health} />))}
           </ScrollView>
         </View>
       </ScrollView>
