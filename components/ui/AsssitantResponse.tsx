ebee07139f919e666047d77aa2fac6103d71931b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-04-23 18:16:55 +0700

    refactor AssistantResponse component for improved structure and readability

--- components/ui/AsssitantResponse.tsx
+++ components/ui/AsssitantResponse.tsx
@@ -1,112 +1,29 @@
 /* eslint-disable import/no-unresolved */
-import { AIResponse } from '@/components/AIResponse';
 import { CustomText } from '@/components/Text';
 import { CustomTextInput } from '@/components/TextInput';
-import { ThemedView } from '@/components/ThemedView';
 import { AI_ASSISTANTS } from '@/constants/assistants';
 import { Dispatch, SetStateAction, useEffect } from 'react';
-import { ActivityIndicator, View } from 'react-native';
+import { View } from 'react-native';
 import { CustomButton } from '../Button';
 import { useLocalSearchParams } from 'expo-router';
 import { useGenerateAssistant } from '@/hooks/useGenerateAssistant';
+import { AssistantResponseCompound } from './AssistantResponseCompound';
 
 function AssistantForm() {
   const { assistantType } = useLocalSearchParams();
-  console.log(assistantType);
-
-  const { object } = useGenerateAssistant();
-  const responseSchema = () => {
-    if (assistantType === 'social') {
-      return {
-        title: object?.title,
-        content: object?.content,
-        thoughts: object?.thoughts,
-      };
-    }
-    if (assistantType === 'health') {
-      return {
-        diagnosis: object?.diagnosis,
-        recommendedMedications: object?.recommendedMedications,
-        precautions: object?.precautions,
-        followUp: object?.followUp,
-      };
-    }
-  };
-  const response = responseSchema();
 
   if (assistantType === 'social') {
-    return (
-      <>
-        <View style={{ gap: 8 }}>
-          <CustomText style={{ paddingHorizontal: 24 }}>Content Title</CustomText>
-          <ThemedView type="assistant" style={{ paddingHorizontal: 24, borderRadius: 12 }}>
-            <AIResponse>{response?.title !== undefined ? response.title : 'Recommended Title Content'}</AIResponse>
-          </ThemedView>
-        </View>
-
-        <View style={{ gap: 8 }}>
-          <CustomText style={{ paddingHorizontal: 24 }}>Content Description</CustomText>
-          <ThemedView type="assistant" style={{ paddingHorizontal: 24, borderRadius: 12 }}>
-            <AIResponse>{response?.content !== undefined ? response.content : 'Description'}</AIResponse>
-          </ThemedView>
-        </View>
-
-        <View style={{ gap: 8 }}>
-          <CustomText style={{ paddingHorizontal: 24 }}>Thoughts on this content</CustomText>
-          <ThemedView type="assistant" style={{ paddingHorizontal: 24, borderRadius: 12 }}>
-            <AIResponse>{response?.thoughts !== undefined ? response.thoughts : 'Thoughts About Content'}</AIResponse>
-          </ThemedView>
-        </View>
-      </>
-    );
+    return <AssistantResponseCompound.ContentAssistantResponse />;
   }
+
   if (assistantType === 'health') {
-    return (
-      <>
-        <View style={{ gap: 8 }}>
-          <CustomText style={{ paddingHorizontal: 24 }}>Diagnosis</CustomText>
-          <ThemedView type="assistant" style={{ paddingHorizontal: 24, borderRadius: 12 }}>
-            <AIResponse>{response?.diagnosis !== undefined ? response.diagnosis : 'Summary of condition'}</AIResponse>
-          </ThemedView>
-        </View>
-
-        <View style={{ gap: 8 }}>
-          <CustomText style={{ paddingHorizontal: 24 }}>Recommended Medications</CustomText>
-          <ThemedView type="assistant" style={{ paddingHorizontal: 24, borderRadius: 12 }}>
-            {response?.recommendedMedications === undefined && <AIResponse>List of Medications</AIResponse>}
-            {response?.recommendedMedications?.map((med: any, index: number) => (
-              <View key={index} style={{ borderColor: 'white', gap: 0 }}>
-                {/* Divider line */}
-                {index >= 1 && <View style={{ flex: 1, height: 1, borderRadius: 99, backgroundColor: '#5e5e5e' }} />}
-                <AIResponse>{`Name: ${med?.name}`}</AIResponse>
-                <AIResponse>{`Dosage: ${med?.dosage}`}</AIResponse>
-                <AIResponse>{`Frequency: ${med?.frequency}`}</AIResponse>
-              </View>
-            ))}
-          </ThemedView>
-        </View>
-
-        <View style={{ gap: 8 }}>
-          <CustomText style={{ paddingHorizontal: 24 }}>Precations</CustomText>
-          <ThemedView type="assistant" style={{ paddingHorizontal: 24, borderRadius: 12 }}>
-            <AIResponse>{response?.precautions !== undefined ? response.precautions : 'Precautions related to the diagnosis'}</AIResponse>
-          </ThemedView>
-        </View>
-
-        <View style={{ gap: 8 }}>
-          <CustomText style={{ paddingHorizontal: 24 }}>Follow Up</CustomText>
-          <ThemedView type="assistant" style={{ paddingHorizontal: 24, borderRadius: 12 }}>
-            <AIResponse>{response?.followUp !== undefined ? response.followUp : 'Suggestion for further consultation'}</AIResponse>
-          </ThemedView>
-        </View>
-      </>
-    );
+    return <AssistantResponseCompound.HealthAssistantResponse />;
   }
 }
 
 export function AssistantResponse({ input, setInput }: { input: string; setInput: Dispatch<SetStateAction<string>> }) {
   const { assistantId, assistantType } = useLocalSearchParams();
-  const { setSchemaType, isLoading, submit, stop } = useGenerateAssistant();
+  const { isLoading, submit, stop } = useGenerateAssistant();
 
   const assistant = AI_ASSISTANTS.map((item) => {
     if (assistantType === 'social') return item.socialMedia.filter((item) => item.type === assistantId)[0];
@@ -116,15 +33,12 @@ export function AssistantResponse({ input, setInput }: { input: string; setInput
   console.log(assistant);
 
   useEffect(() => {
-    setSchemaType(assistantType.toString());
     console.log('inside modal');
     return () => {
-      console.log('outside modal');
-      stop(); // Stop any ongoing processes
-      setInput(''); // Reset the input state
+      console.log('ouside modal');
+      stop();
     };
-  }, [stop, setInput, assistantType, setSchemaType]);
-
+  }, [stop]);
   return (
     <>
       <View style={{ gap: 14 }}>
@@ -136,13 +50,9 @@ export function AssistantResponse({ input, setInput }: { input: string; setInput
         >
           <CustomText style={{ textAlign: 'center' }}>Generate</CustomText>
         </CustomButton>
-
         {isLoading && (
-          <CustomButton lightColor="#161616" darkColor="white" style={{ flexDirection: 'row', gap: 10, alignItems: 'center', justifyContent: 'center', padding: 8, borderRadius: 12, overflow: 'hidden' }} onPress={() => stop()}>
-            <CustomText lightColor="white" darkColor="black">
-              Stop
-            </CustomText>
-            <ActivityIndicator />
+          <CustomButton style={{ padding: 8, borderRadius: 12, overflow: 'hidden' }}>
+            <CustomText>Stop</CustomText>
           </CustomButton>
         )}
       </View>
@@ -150,7 +60,7 @@ export function AssistantResponse({ input, setInput }: { input: string; setInput
       {/* divider line */}
       <View style={{ marginVertical: 14, marginHorizontal: 10, flex: 1, height: 1, borderRadius: 99, backgroundColor: '#5e5e5e' }} />
 
-      <View style={{ paddingBottom: 80, display: 'flex', gap: 14 }}>
+      <View style={{ display: 'flex', gap: 14 }}>
         <AssistantForm />
       </View>
     </>
