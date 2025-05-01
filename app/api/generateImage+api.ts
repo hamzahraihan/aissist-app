52d37a6824ff9f9ef73323a6d5b759b79bccf16d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-05-01 11:01:29 +0700

    refactor: clean up commented-out code in generateImage API

--- app/api/generateImage+api.ts
+++ app/api/generateImage+api.ts
@@ -1,13 +1,14 @@
-import { Cloudflare } from 'cloudflare';
+// import Cloudflare from 'cloudflare';
 
-const client = new Cloudflare({
-  apiToken: process.env.EXPO_PUBLIC_CLOUDFLARE_API_TOKEN,
-  apiKey: process.env.EXPO_PUBLIC_CLOUDFLARE_API_KEY,
-});
+// const client = new Cloudflare({
+//   apiToken: process.env.EXPO_PUBLIC_CLOUDFLARE_API_TOKEN,
+//   apiKey: process.env.EXPO_PUBLIC_CLOUDFLARE_API_KEY,
+// });
 
 export async function POST(req: Request) {
   const { prompt, modelName }: { prompt: string; modelName: string } = await req.json();
   console.log(prompt);
+
   if (!prompt) {
     return new Response('No prompt provided', { status: 400 });
   }
@@ -15,17 +16,30 @@ export async function POST(req: Request) {
   try {
     console.log(`generating image with ${modelName}`);
 
-    const response = await client.ai.run(modelName, {
-      account_id: '9b37eea8034fbf61191d273e000f450e',
-      prompt: prompt,
-    });
-
-    console.log(response);
-    return Response.json(response, {
+    const response = await fetch(process.env.EXPO_PUBLIC_CLOUDFLARE_BASE_URL + '/generate-image' || '', {
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
+      body: JSON.stringify({ model: modelName, prompt }),
     });
+
+    return Response.json(await response.json(), {
+      headers: {
+        'Content-Type': 'image/png',
+      },
+    });
+    // const response = await client.ai.run(modelName, {
+    //   account_id: '9b37eea8034fbf61191d273e000f450e',
+    //   prompt: prompt || 'cyberpunk cat',
+    // });
+
+    // console.log(response);
+    // return Response.json(response, {
+    //   headers: {
+    //     'Content-Type': 'image/png',
+    //   },
+    // });
   } catch (error) {
     console.error(error);
     return new Response('Error', { status: 500 });
