d346328797605f8b56a20a8bc971fc8b55008414
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-05-01 11:01:39 +0700

    refactor: remove unused imports and clean up code in workers.ts

--- services/workers.ts
+++ services/workers.ts
@@ -1,5 +1,8 @@
+// import Cloudflare from 'cloudflare';
 import { createWorkersAI } from 'workers-ai-provider';
 import { streamText } from 'ai';
+import { Ai } from '@cloudflare/ai';
+// const client = new Cloudflare();
 
 type Env = {
   AI: Ai;
@@ -7,35 +10,48 @@ type Env = {
 
 export default {
   async fetch(req: Request, env: Env) {
+    const url = new URL(req.url);
+    const { model, prompt }: { model: any; prompt: string } = await req.json();
+
     if (req.method !== 'POST') {
       return new Response('Only POST allowed', { status: 405 });
     }
 
-    let body: { model: any; prompt: string };
-
-    try {
-      body = await req.json();
-    } catch {
-      return new Response('Invalid JSON', { status: 400 });
-    }
-
-    const { model = '@cf/deepseek-ai/deepseek-r1-distill-qwen-32b', prompt = 'Write a 50-word essay about hello world.' } = body;
+    const workersai = createWorkersAI({ binding: env.AI });
 
     if (!prompt) {
       return new Response('No prompt provided', { status: 400 });
     }
 
-    const workersai = createWorkersAI({ binding: env.AI });
-    const result = streamText({
-      model: workersai(model),
-      prompt,
-    });
-    return result.toTextStreamResponse({
-      headers: {
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        Connection: 'keep-alive',
-      },
-    });
+    if (url.pathname === '/chat') {
+      /*       const { model = '@cf/deepseek-ai/deepseek-r1-distill-qwen-32b', prompt = 'Write a 50-word essay about hello world.' } = body;
+       */
+      const result = streamText({
+        model: workersai(model || '@cf/deepseek-ai/deepseek-r1-distill-qwen-32b'),
+        prompt,
+      });
+
+      return result.toTextStreamResponse({
+        headers: {
+          'Content-Type': 'text/event-stream',
+          'Cache-Control': 'no-cache',
+          Connection: 'keep-alive',
+        },
+      });
+    }
+
+    if (url.pathname === '/generate-image') {
+      const ai = new Ai(env.AI);
+
+      const response = await ai.run(model, {
+        prompt: prompt || 'cyberpunk cat',
+      });
+
+      return Response.json(response, {
+        headers: {
+          'Content-Type': 'image/png',
+        },
+      });
+    }
   },
 };
