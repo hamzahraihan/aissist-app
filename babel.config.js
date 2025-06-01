af614111431ab524fc396df4b794bf4fa942f1b5
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-06-01 22:00:04 +0700

    enhance babel configuration with module resolver and extensions

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,30 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "react-native-reanimated/plugin",
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          extensions: [
+            ".ios.ts",
+            ".android.ts",
+            ".ts",
+            ".ios.tsx",
+            ".android.tsx",
+            ".tsx",
+            ".jsx",
+            ".js",
+            ".json",
+          ],
+          alias: {
+            "@": "./*",
+            // Add other aliases as needed
+          },
+        },
+      ],
+    ],
   };
 };
