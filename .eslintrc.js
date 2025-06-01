7e5208eb3ae9d4362daf1499618d05d41949464a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-06-01 21:59:58 +0700

    refactor ESLint configuration for improved readability

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,20 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ['expo', 'plugin:import/typescript', 'plugin:import/warnings', 'plugin:import/errors'],
-  ignorePatterns: ['/dist/*'],
+  extends: [
+    "expo",
+    "plugin:import/typescript",
+    "plugin:import/warnings",
+    "plugin:import/errors",
+  ],
+  ignorePatterns: ["/dist/*"],
   settings: {
-    'import/resolver': {
-      node: {
-        path: ['app'],
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        moduleDirectory: ['node_modules', 'app/'],
-      },
+    "import/resolver": {
       typescript: {},
+      node: {
+        path: ["app"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        moduleDirectory: ["node_modules", "app/"],
+      },
     },
   },
 };
